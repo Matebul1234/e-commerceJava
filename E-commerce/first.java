0e7eb6ce1aeae43a4fa81a94c97332eaae7f74f8
Matebul1234 <mohammadmatebul047890gmail.com>
2023-09-13 20:17:53 +0530

    this is me

--- E-commerce/first.java
+++ E-commerce/first.java
@@ -1,4 +1,7 @@
 class First{
+    public static void getfile(){
+        System.out.println("this is method for testing");
+    }
     public static void main(String arg[]){
         System.out.println("hello ");
     }
