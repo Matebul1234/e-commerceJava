74ac5240a15ed9ed31064196edb7846bb9fafd21
Matebul1234 <mohammadmatebul047890gmail.com>
2023-09-13 20:22:48 +0530

    this is changes

--- E-commerce/first.java
+++ E-commerce/first.java
@@ -1,6 +1,7 @@
 class First{
     public static void getfile(){
         System.out.println("this is method for testing");
+        System.out.print("thisisi");
     }
     public static void main(String arg[]){
         System.out.println("hello ");
